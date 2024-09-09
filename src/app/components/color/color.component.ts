5b15afe26826febf97fb7fb80b116ca2490843e4
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-09 13:16:50 +0100

    correction colorComponent

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
-  styleUrls: ['./color.component.css']
+  styleUrls: ['./color.component.css'],
 })
 export class ColorComponent {
-  color = 'red';
+  // State
+  defaultColor = 'red';
+  color = this.defaultColor;
+
+  // coùportements
+  changeColor(newColorInput: HTMLInputElement): void {
+    this.color = newColorInput.value;
+    newColorInput.value = '';
+  }
+
+  reset(): void {
+    this.color = this.defaultColor;
+  }
 }
