49677dd4ea786ea32fd1ba5c0fa2203bc97cdcc3
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-09 12:48:31 +0100

    init color exemple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
+import { ColorComponent } from './components/color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
