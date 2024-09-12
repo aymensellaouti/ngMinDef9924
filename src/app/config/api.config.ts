0e65fb06f575d7d0e3a9859c71bd3034d74e3aa7
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-12 10:00:16 +0100

    add ressources

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,5 @@
 export const APP_API = {
   todo: 'https://jsonplaceholder.typicode.com/todos',
+  cv: 'https://apilb.tridevs.net/api/personnes/',
+  formationSwagger: 'https://apilb.tridevs.net/explorer/#/personne',
 };
